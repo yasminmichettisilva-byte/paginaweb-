ea05be22cffd5fa608093f39268352bd655f048c
yasminmichettisilva-byte <yasmin.michetti.silva@escola.pr.gov.br>
2026-08-13 12:20:39 -0300

    13-08

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-button{
-    padding: 10px 20px;
-    font-size: 18px;
-    cursor: pointer;
-}
-const botao = document.getElementByld("tema");
+const botao = document.getElementById("tema");
 
 botao.addEventListener("click", function(){
     document.body.classList.toggle("dark");
